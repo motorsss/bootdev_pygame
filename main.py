f987ec07a526a5628c5d9c514f93571b65158694
motorsss <mehmet_zahit@hotmail.com>
2024-08-22 21:49:01 +0300

    added player class and rendered in window.

--- main.py
+++ main.py
@@ -1,37 +1,29 @@
-# this allows us to use code from
-# the open-source pygame library
-# 
-# throughout this file
 import pygame
 from constants import *
+from player import *
+
 Black = (0, 0, 0)
 
 def main():
-    print("Starting asteroids!")
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    font = pygame.font.SysFont("Arial" , 18 , bold = True)
-
-    def update_fps():
-        fps = str(int(clock.get_fps()))
-        fps_text = font.render(fps, True, pygame.Color("red"))
-        return fps_text
-
-
+    font = pygame.font.SysFont("Arial" , 28 , bold = True)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(Black)
-        screen.blit(update_fps(), (10,10))
+        player.draw(screen)
+        fps = str(int(clock.get_fps()))
+        fps_text = font.render(fps, True, pygame.Color("red"))
+        screen.blit(fps_text, (10,10))
         pygame.display.flip()
         dt = clock.tick(60)/1000
-        
 
 if __name__== "__main__":
     main()

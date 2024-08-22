6775d2f2e737b2306f6fb7388742502edba0d481
motorsss <mehmet_zahit@hotmail.com>
2024-08-22 17:28:11 +0300

    FPS counter

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 import pygame
 from constants import *
 Black = (0, 0, 0)
+
 def main():
     print("Starting asteroids!")
     print(f'Screen width: {SCREEN_WIDTH}')
@@ -12,12 +13,25 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    font = pygame.font.SysFont("Arial" , 18 , bold = True)
+
+    def update_fps():
+        fps = str(int(clock.get_fps()))
+        fps_text = font.render(fps, True, pygame.Color("red"))
+        return fps_text
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(Black)
+        screen.blit(update_fps(), (10,10))
         pygame.display.flip()
+        dt = clock.tick(60)/1000
         
+
 if __name__== "__main__":
     main()
